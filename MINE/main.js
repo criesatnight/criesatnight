c3c0c6c0111eed8fcfe84e5185094738c6e245fa
criesatnight <98652743+criesatnight@users.noreply.github.com>
2022-01-29 23:34:47 +0800

    applause
    no mobile mode

---
+++ MINE/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "200px";
+    document.getElementById("menu").style.marginLeft = "10px";
+  }
+  
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("menu").style.marginLeft= "0";
+  }
